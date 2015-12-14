851c7ccb8de820f886e675819b8251a438a389cf
thomasvaeth <thomas.vaeth@gmail.com>
2015-12-14 15:49:36 -0800

    Smooth scroll.

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('SamesiesApplication', ['ngRoute']);
+var app = angular.module('SamesiesApplication', ['ngRoute', 'smoothScroll']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider
